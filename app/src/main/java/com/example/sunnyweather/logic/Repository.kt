2b1e580718494a81460f5745b7c73fe72c920d00
朱凌霄 <larry_zhu@foxmail.com>
2021-09-08 00:42:55 +0800

    feat: 完成ui界面编写

--- app/src/main/java/com/example/sunnyweather/logic/Repository.kt
+++ app/src/main/java/com/example/sunnyweather/logic/Repository.kt
@@ -4,23 +4,23 @@ import androidx.lifecycle.liveData
 import com.example.sunnyweather.logic.model.Place
 import com.example.sunnyweather.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
-import okhttp3.Dispatcher
-import java.lang.Exception
 import java.lang.RuntimeException
 
 object Repository {
+
   fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
     val result = try {
-        val placeRes = SunnyWeatherNetwork.searchPlaces(query)
-        if(placeRes.status == "ok"){
-          val places = placeRes.places
-          Result.success(places)
-        }else{
-          Result.failure(RuntimeException("res status is ${placeRes.status}"))
-        }
-    } catch (e: Exception){
+      val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+      if (placeResponse.status == "ok") {
+        val places = placeResponse.places
+        Result.success(places)
+      } else {
+        Result.failure(RuntimeException("response status is $placeResponse.status"))
+      }
+    } catch (e: Exception) {
       Result.failure<List<Place>>(e)
     }
-    emit(result as Dispatchers)
+    emit(result)
   }
+
 }
