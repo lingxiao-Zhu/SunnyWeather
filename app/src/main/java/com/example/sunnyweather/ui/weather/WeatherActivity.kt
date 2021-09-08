9dc62af5e7b86e1746f01651e37876c75730b403
朱凌霄 <larry_zhu@foxmail.com>
2021-09-09 00:33:35 +0800

    feat: 完成UI编写

---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sunnyweather.ui.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sunnyweather.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
